71f16ca0421ba4b56f363a7910053472449743b7
c-loob <chrstrloob@gmail.com>
2015-03-22 02:05:35 +0200

    fb login
    fb login JS ja php sümbioos
    much fun
    vt test4

---
+++ test4/js.js
@@ -0,0 +1,57 @@
+
+function login() {
+    "use strict";
+    document.cookie = 'fb_token=' + FB.getAuthResponse().accessToken;
+    appPropagation();
+}
+
+function logout() {
+    "use strict";
+    document.cookie = 'fb_token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    appPropagation();
+}
+
+function loginStatusChangeCallback(response) {
+    "use strict";
+
+    if (response.status === 'connected') {
+        login();
+    } else {
+        logout();
+    }
+}
+
+function checkLoginState() {
+    "use strict";
+
+    FB.getLoginStatus(function (response) {
+        loginStatusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function () {
+    "use strict";
+
+    FB.init({
+        appId      : '1604288763142467',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.0'
+    });
+
+    checkLoginState();
+
+};
+
+(function (d, s, id) {
+    "use strict";
+
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
