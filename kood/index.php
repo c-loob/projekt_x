566997fe63af8803c7376482dd563d152e689f4c
c-loob <chrstrloob@gmail.com>
2015-03-23 00:04:56 +0200

    sad
    asd

--- kood/index.php
+++ kood/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" 
 "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<html>
 <head>
    <title>Projektx - Valime Parimat</title>
    
