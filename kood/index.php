61d981e3e12279666b0470e45b9c9d9d5985c536
mihkelkaasik <mihkelk@ut.ee>
2015-04-05 22:10:57 +0300

    html fix

--- kood/index.php
+++ kood/index.php
@@ -34,7 +34,7 @@
 		 	<script type="text/javascript" src="send_sse.js"></script>
 		 </div>
          <div class='leftside' id='navigationWrapper'>
-            <a href="kandidaadid.php" class="menuButton" class="menuButtonClicked" id="kandidaadid">Kandidaadid</a>
+            <a href="kandidaadid.php" class="menuButton menuButtonClicked" id="kandidaadid">Kandidaadid</a>
             <a href="erakondadehaaltearv.php" class="menuButton" id="erakondadehaaltearv">Tulemused</a>
             <a href="statistika.php" class="menuButton" id="statistika">Statistika</a>
          </div>
@@ -55,7 +55,7 @@
                <div class='subContent'>
                   <p>Siia tuleb "HÃ¤Ã¤letama" nupp</p>
                </div>
-               <div class="lisaKandidaadiks" class='subContent' >
+               <div class="lisaKandidaadiks subContent" >
                	<a href="lisaKandidaadiks.php" class="menuButton" id="lisaKandidaadiks">Lisa kandidaadiks</a>
                </div>
             </div>
