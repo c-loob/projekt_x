1f85b3423dcb57bd56bd5c287d0fe0c234dde653
c-loob <chrstrloob@gmail.com>
2015-04-05 13:55:49 +0300

    Revert "Revert "minor""
    This reverts commit b7fddb35c459a093f362b6333f6b912d0174dd4b.

--- kood/statistika.php
+++ kood/statistika.php
@@ -10,7 +10,7 @@
    <link rel="stylesheet" type="text/css" href="styles.css">
    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
    <script src="scripts.js"></script>
-   <script src="facebook.js" type="text/javascript"></script>
+   <script src="js.js" type="text/javascript"></script>
 </head>
 
 <body>
