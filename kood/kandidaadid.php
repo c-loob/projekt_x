7bc32281c208fc8f6cfab873e624cdfddea4fdb9
c-loob <chrstrloob@gmail.com>
2015-04-05 13:53:33 +0300

    Revert "offline"
    This reverts commit 2fb05483150eaa07015f628525dbe45ca8c9c1d3.

--- kood/kandidaadid.php
+++ kood/kandidaadid.php
@@ -3,7 +3,7 @@
  ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" 
 "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" manifest="site.manifest">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
    <Title>Projektx - Valime Parimat</Title>
    <meta charset="UTF-8"> 
