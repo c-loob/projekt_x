893c473472c7618f7e31f9bd1ba1285acf8f15ce
mihkelkaasik <mihkelk@ut.ee>
2015-05-22 15:03:30 +0300

    statfix

--- kood/statistika.php
+++ kood/statistika.php
@@ -123,7 +123,8 @@
       </div>
 
    </div>
-
+    	<!-- Include all compiled plugins (below), or include individual files as needed -->
+    	<script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
