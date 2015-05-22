e8f6a0189bb19120d7dde6c5768daa807d7326a2
c-loob <chrstrloob@gmail.com>
2015-05-22 08:58:02 -0700

    Update lisaKandidaadiks.php

--- kood/lisaKandidaadiks.php
+++ kood/lisaKandidaadiks.php
@@ -77,7 +77,7 @@
   											 </div>';*/
   						echo   	'<label for="inputEesnimi" class="col-sm-2 control-label">Eesnimi</label>';
   						echo	'<div class="col-sm-10">
-  							"tere"
+  							$_SESSION['user_firstname']
   							</div>';
       						echo   	'<div class="form-group">
     											<label for="inputPerenimi" class="col-sm-2 control-label">Perenimi</label>
