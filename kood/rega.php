9d1d03b1776465f97db1eff9d4b9680a54e16996
c-loob <chrstrloob@gmail.com>
2015-05-23 09:47:07 -0700

    Update rega.php

--- kood/rega.php
+++ kood/rega.php
@@ -9,10 +9,7 @@
         // Info andmebaasi
        // $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
          //          VALUES (?,?)";
-        $sql_insert = "IF NOT EXISTS (SELECT * FROM Kasutajad WHERE eesnimi = '$eesnimi' AND perenimi = '$perenimi')
-		BEGIN
-			INSERT INTO Kasutajad (eesnimi, perenimi) VALUES (?,?) 
-		END";
+        $sql_insert = "REPLACE INTO kasutajad SET eesnimi=?, perenimi=?; 
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $eesnimi);
         $stmt->bindValue(2, $perenimi);
