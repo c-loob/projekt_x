d8acfe0216624e2a4f9411a534ce1ecb738157d1
c-loob <chrstrloob@gmail.com>
2015-05-23 09:07:01 -0700

    Update rega.php

--- kood/rega.php
+++ kood/rega.php
@@ -7,8 +7,12 @@
         $eesnimi = $_SESSION['user_firstname'];
         $perenimi = $_SESSION['user_lastname'];
         // Info andmebaasi
-        $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
-                   VALUES (?,?)";
+       // $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
+         //          VALUES (?,?)";
+        $sql_insert = "IF NOT EXISTS (SELECT * FROM Kasutajad WHERE eesnimi = '$eesnimi' AND perenimi = '$perenimi')
+		BEGIN
+			INSERT INTO Kasutajad (eesnimi, perenimi) VALUES (?,?) 
+		END";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $eesnimi);
         $stmt->bindValue(2, $perenimi);
