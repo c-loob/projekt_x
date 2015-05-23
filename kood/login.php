c8c2276036b6d426144c5f783a4c49b10611fc85
c-loob <chrstrloob@gmail.com>
2015-05-23 04:48:53 -0700

    Update login.php

--- kood/login.php
+++ kood/login.php
@@ -1,14 +1,14 @@
 <?php 
 function AfterLogin(){//kui toimub logimise olekumuutus
 	window.location.reload();
-	if(!isset($_COOKIE['fb_token'])) {
+	//if(!isset($_COOKIE['fb_token'])) {
 		$eesnimi = $_SESSION['user_firstname'];
 		$perenimi = $_SESSION['user_lastname'];
 	//lisatakse kasutajate tabelisse kui pole juba lisatud
 		$sql_insert = "IF NOT EXISTS (SELECT * FROM Kasutajad WHERE eesnimi = '$eesnimi' AND perenimi = '$perenimi')
 			BEGIN
 				INSERT INTO Kasutajad (eesnimi, perenimi) VALUES ('$eesnimi', '$perenimi') 
-			END"};
+			END";
 
 }
 
