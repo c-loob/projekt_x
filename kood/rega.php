6863a14a114066e8a7bcb17c4151a6f1629f20da
c-loob <chrstrloob@gmail.com>
2015-05-23 08:35:24 -0700

    Create rega.php

---
+++ kood/rega.php
@@ -0,0 +1,23 @@
+<?php
+	include 'dbcon.php';
+    	include 'login.php';
+    // Salvesta info
+    
+    try {
+        $eesnimi = $_SESSION['user_firstname'];
+        $perenimi = $_SESSION['user_lastname'];
+        // Info andmebaasi
+        $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
+                   VALUES (?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $eesnimi);
+        $stmt->bindValue(2, $perenimi);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    
+    closemysql();
+    header('location:index.php');
+?>
