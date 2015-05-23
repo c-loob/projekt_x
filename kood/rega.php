257c4713aa8e941727909beafbbb6dc461bc38d8
c-loob <chrstrloob@gmail.com>
2015-05-23 08:52:40 -0700

    Update rega.php

--- kood/rega.php
+++ kood/rega.php
@@ -4,15 +4,19 @@
     // Salvesta info
     
     try {
+    	echo '1';
         $eesnimi = $_SESSION['user_firstname'];
         $perenimi = $_SESSION['user_lastname'];
         // Info andmebaasi
+        echo '2';
         $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
                    VALUES (?,?)";
         $stmt = $conn->prepare($sql_insert);
+        echo "3";
         $stmt->bindValue(1, '$eesnimi');
         $stmt->bindValue(2, '$perenimi');
         $stmt->execute();
+        echo "fin";
     }
     catch(Exception $e) {
         die(var_dump($e));
