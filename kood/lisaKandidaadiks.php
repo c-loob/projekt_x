4c9250f078e49c0f22fbafd5b561672f9dab97e2
c-loob <chrstrloob@gmail.com>
2015-05-24 10:22:29 -0700

    Update lisaKandidaadiks.php

--- kood/lisaKandidaadiks.php
+++ kood/lisaKandidaadiks.php
@@ -41,9 +41,9 @@
               			<li><a href="statistika.php" class="btn btn-lg btn-default" id="statistika">Statistika</a></li>
             		</ul>
             		<ul class="nav navbar-nav navbar-right">
-            			<li><?php 
-            			echo '<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true" onlogin="window.location.reload()"></div>'; 
-            			?></li>
+            			<li> 
+            		 '<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true" onlogin="window.location.reload()"></div>'; 
+            			</li>
             		</ul>
           		</div><!--/.nav-collapse -->
         		</div><!--/.container-fluid -->
