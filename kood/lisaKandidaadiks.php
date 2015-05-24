d8e71e774c9f07f54746f8e84e857e33bada36eb
c-loob <chrstrloob@gmail.com>
2015-05-24 10:18:21 -0700

    Update lisaKandidaadiks.php

--- kood/lisaKandidaadiks.php
+++ kood/lisaKandidaadiks.php
@@ -42,7 +42,7 @@
             		</ul>
             		<ul class="nav navbar-nav navbar-right">
             			<li><?php 
-            			echo '<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true" onlogin="window.location.href = window.location.href"></div>'; 
+            			echo '<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true" onlogin="window.location.reload(True)"></div>'; 
             			?></li>
             		</ul>
           		</div><!--/.nav-collapse -->
