f0cb35e256591700b0f8860b23ce0a75c2cf5533
c-loob <chrstrloob@gmail.com>
2015-05-24 05:10:58 -0700

    Update lisaKandidaadiksAdd.php

--- kood/lisaKandidaadiksAdd.php
+++ kood/lisaKandidaadiksAdd.php
@@ -9,16 +9,18 @@
     try {
         $eesnimi = $_SESSION['user_firstname'];
         $perenimi = $_SESSION['user_lastname'];
+        $uid = $_SESSION['user_id'];
         $piirkond = $_POST['piirkond'];
         $erakond = $_POST['erakond'];
         // Info andmebaasi
         
-        $sql_insert = "REPLACE INTO kandidaadid SET eesnimi=?, perenimi=?, piirkond=?, erakond=?, haali=0"; 
+        $sql_insert = "REPLACE INTO kandidaadid SET eesnimi=?, perenimi=?, piirkond=?, erakond=?, haali=0, uid?"; 
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $eesnimi);
         $stmt->bindValue(2, $perenimi);
         $stmt->bindValue(3, $piirkond);
         $stmt->bindValue(4, $erakond);
+        $stmt->bindValue(5, $uid);
         $stmt->execute();
     }
     catch(Exception $e) {
