f835e3ca30b8842fee947ea436ce8d7cc3401d04
c-loob <chrstrloob@gmail.com>
2015-05-24 04:54:10 -0700

    Update rega.php

--- kood/rega.php
+++ kood/rega.php
@@ -7,13 +7,15 @@
 	    try {
 	        $eesnimi = $_SESSION['user_firstname'];
 	        $perenimi = $_SESSION['user_lastname'];
+	        $uid = $_SESSION['user_id'];
 	        // Info andmebaasi
 	       // $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
 	         //          VALUES (?,?)";
-	        $sql_insert = "REPLACE INTO kasutajad SET eesnimi=?, perenimi=?"; 
+	        $sql_insert = "REPLACE INTO kasutajad SET eesnimi=?, perenimi=?, id=?"; 
 	        $stmt = $conn->prepare($sql_insert);
 	        $stmt->bindValue(1, $eesnimi);
 	        $stmt->bindValue(2, $perenimi);
+	        $stmt->bindValue(3, $uid);
 	        $stmt->execute();
 	    }
 	    catch(Exception $e) {
