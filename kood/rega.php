2fd7328bb3f5b214491f6f30c34e67e7528d059e
c-loob <chrstrloob@gmail.com>
2015-05-24 04:51:36 -0700

    Update rega.php

--- kood/rega.php
+++ kood/rega.php
@@ -3,21 +3,23 @@
 	include 'dbcon.php';
     	include 'login.php';
     // Salvesta info
-    try {
-        $eesnimi = $_SESSION['user_firstname'];
-        $perenimi = $_SESSION['user_lastname'];
-        // Info andmebaasi
-       // $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
-         //          VALUES (?,?)";
-        $sql_insert = "REPLACE INTO kasutajad SET eesnimi=?, perenimi=?"; 
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $eesnimi);
-        $stmt->bindValue(2, $perenimi);
-        $stmt->execute();
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-    closemysql();
+    if(isset($_COOKIE['fb_token'])){ 
+	    try {
+	        $eesnimi = $_SESSION['user_firstname'];
+	        $perenimi = $_SESSION['user_lastname'];
+	        // Info andmebaasi
+	       // $sql_insert = "INSERT INTO kasutajad (eesnimi, perenimi) 
+	         //          VALUES (?,?)";
+	        $sql_insert = "REPLACE INTO kasutajad SET eesnimi=?, perenimi=?"; 
+	        $stmt = $conn->prepare($sql_insert);
+	        $stmt->bindValue(1, $eesnimi);
+	        $stmt->bindValue(2, $perenimi);
+	        $stmt->execute();
+	    }
+	    catch(Exception $e) {
+	        die(var_dump($e));
+	    }
+	    closemysql();
+    };
     //header('location:index.php');
 ?>
